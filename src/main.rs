c67c0125075ad8d5c6d1ad80510a444daa625bb6
Adrien Loxq <adrien.loxq@gmail.com>
2023-02-18 19:08:31 +0100

    [wip] parse brainfuck source code and turn it to opcodes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,42 @@
-fn evalbf(code: &str) -> &str {
+#[derive(PartialEq)]
+enum OpCode {
+    IncPtr,
+    DecPtr,
+    Inc,
+    Dec,
+    Write,
+    Read,
+    LoopStart,
+    LoopStop
+}
+
+fn parsebf(code: &str) -> Vec<OpCode> {
+    let mut operations = Vec::new();
+    
+    for symbol in code.chars() {
+        let operation = match symbol {
+            '>' => Some(OpCode::IncPtr),
+            '<' => Some(OpCode::DecPtr),
+            '+' => Some(OpCode::Inc),
+            '-' => Some(OpCode::Dec),
+            '.' => Some(OpCode::Write),
+            ',' => Some(OpCode::Read),
+            '[' => Some(OpCode::LoopStart),
+            ']' => Some(OpCode::LoopStop),
+            _ => None
+        };
+
+        match operation {
+            Some(operation) => operations.push(operation),
+            None => ()
+        }
+    }
+
+    operations
+}
+
+fn runbf(code: &str) -> &str {
+    
     ""
 }
 
@@ -10,7 +48,26 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_evalbf() {
+    fn test_parsebf() {
+        let payload = ".-+[]> <#,";  // all invalid characters (space and '#') should be ignored
+    
+        let output = parsebf(payload);
+        let canary = &Vec::from([
+            OpCode::Write,
+            OpCode::Dec,
+            OpCode::Inc,
+            OpCode::LoopStart,
+            OpCode::LoopStop,
+            OpCode::IncPtr,
+            OpCode::DecPtr,
+            OpCode::Read
+        ]);
+
+        assert!(output.len() == canary.len() && output.iter().zip(canary).all(|(a, b)| *a == *b))
+    }
+
+    //#[test]
+    fn test_runbf() {
         let helloworld = "++++++++++
             [
                 >+++++++
@@ -20,7 +77,7 @@ mod test {
             ]   >++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.
             --------.>+.>.";
     
-        let output = evalbf(helloworld);
+        let output = runbf(helloworld);
         println!("> {}", output);
         assert_eq!("Hello World!", output);
     }
