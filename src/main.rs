57f1ad716018752e54c34b967961fc6a00b430dc
Adrien Loxq <adrien.loxq@gmail.com>
2023-02-18 22:48:46 +0100

    [feat] build instructions from opcode including loop scopes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[derive(PartialEq)]
+#[derive(Clone)]
+#[derive(Debug)]
 enum OpCode {
     IncPtr,
     DecPtr,
@@ -8,6 +10,18 @@ enum OpCode {
     Read,
     LoopStart,
     LoopStop
+}
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+enum Instruction {
+    IncPtr,
+    DecPtr,
+    Inc,
+    Dec,
+    Write,
+    Read,
+    Loop(Vec<Instruction>)
 }
 
 fn lex(code: &str) -> Vec<OpCode> {
@@ -39,6 +53,54 @@ fn lex(code: &str) -> Vec<OpCode> {
     operations
 }
 
+fn parsebf(operations: Vec<OpCode>) -> Vec<Instruction> {
+    let mut program: Vec<Instruction> = Vec::new();
+    let mut loop_start: usize = 0;
+    let mut loop_stack: i32 = 0;
+
+    for (i, operation) in operations.iter().enumerate() {
+        if loop_stack == 0 {
+            let instruct = match operation {
+                OpCode::IncPtr => Some(Instruction::IncPtr),
+                OpCode::DecPtr => Some(Instruction::DecPtr),
+                OpCode::Inc => Some(Instruction::Inc),
+                OpCode::Dec => Some(Instruction::Dec),
+                OpCode::Write => Some(Instruction::Write),
+                OpCode::Read => Some(Instruction::Read),
+                OpCode::LoopStart => {
+                    loop_start = i;
+                    loop_stack += 1;
+                    None
+                },
+                OpCode::LoopStop => panic!("Unbalanced loop end at char #{}", i),
+            };
+            match instruct {
+                Some(instruct) => program.push(instruct),
+                None => ()
+            }
+        } else {
+            match operation {
+                OpCode::LoopStart => {
+                    loop_stack += 1;
+                },
+                OpCode::LoopStop => {
+                    loop_stack -= 1;
+
+                    if loop_stack == 0 {
+                        program.push(Instruction::Loop(parsebf(operations[loop_start+1..i].to_vec())));
+                    }
+                },
+                _ => (),
+            }
+        }
+    }
+    if loop_stack != 0 {
+        panic!("Unbalanced loop start at char #{}", loop_start);
+    }
+
+    program
+}
+
 fn runbf(code: &str) -> &str {
     
     ""
@@ -67,7 +129,35 @@ mod test {
             OpCode::Read
         ]);
 
-        assert!(output.len() == canary.len() && output.iter().zip(canary).all(|(a, b)| *a == *b))
+        assert!(output.len() == canary.len() && output.iter().zip(canary).all(|(a, b)| *a == *b));
+    }
+
+    #[test]
+    fn test_parsebf() {
+        let payload: Vec<OpCode> = Vec::from([
+            OpCode::IncPtr,
+            OpCode::DecPtr,
+            OpCode::Write,
+            OpCode::Read,
+            OpCode::Inc,
+            OpCode::Dec,
+            OpCode::LoopStart,
+            OpCode::Inc,
+            OpCode::Dec,
+            OpCode::LoopStop
+        ]);
+        let canary: &Vec<Instruction> = &Vec::from([
+            Instruction::IncPtr,
+            Instruction::DecPtr,
+            Instruction::Write,
+            Instruction::Read,
+            Instruction::Inc,
+            Instruction::Dec,
+            Instruction::Loop(Vec::from([Instruction::Inc, Instruction::Dec]))
+        ]);
+        let output = parsebf(payload);
+
+        assert!(output.len() == canary.len() && output.iter().zip(canary).all(|(a, b)| a == b));
     }
 
     //#[test]
